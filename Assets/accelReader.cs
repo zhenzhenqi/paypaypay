694dbbb29da2e1620d7b5b4ac550da59bbb2d2df
zhenzhenqi <zhenzhenqi@gmail.com>
2019-12-06 14:34:50 -0500

    first commit

---
+++ Assets/accelReader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class accelReader : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public float accelChange;
+    Vector3 currentA;
+    Vector3 incomingA;
+
+    void Start()
+    {
+        accelChange = 0;
+        currentA = Vector3.zero;
+        incomingA = Vector3.zero;
+        currentA.x = Input.acceleration.x;
+        currentA.y = Input.acceleration.y;
+        currentA.z = Input.acceleration.z;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		incomingA.x = Input.acceleration.x;
+        incomingA.y = Input.acceleration.y;
+        incomingA.z = Input.acceleration.z;
+
+        accelChange = Mathf.Abs(incomingA.sqrMagnitude - currentA.sqrMagnitude);
+
+        currentA.x = Input.acceleration.x;
+        currentA.y = Input.acceleration.y;
+        currentA.z = Input.acceleration.z;
+    
+        Debug.Log(accelChange);
+    }
+}
---
+++ Assets/random_move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class random_move : MonoBehaviour
+{
+    public accelReader accelerationReader;
+    // Start is called before the first frame update
+    Vector2 startingPos;
+	Vector2 endingPos;
+
+    void Start()
+    {
+		startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown("space")){
+        if (accelerationReader.accelChange>0.1f){
+            Vector2 tempDisp = (Vector2)Random.insideUnitCircle;
+//			Debug.Log(tempDisp);
+			endingPos = startingPos + tempDisp;
+			transform.position = endingPos;
+		}
+    }
+}
